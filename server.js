29c124fa16bcc80b74888a9b3def7631b18883d9
Alan Fitzpatrick <alajfit@gmail.com>
2018-02-05 23:06:45 +0000

    Updated the server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const express_graphql = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+// GraphQL schema
+const schema = buildSchema(`
+  type Query {
+      message: String
+  }  
+`)
+
+const root = {
+    message: () => 'Testing 123!'
+}
+
+const app = express()
+app.use('/graphql', express_graphql({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('Running on http://localhost:4000')
+})
